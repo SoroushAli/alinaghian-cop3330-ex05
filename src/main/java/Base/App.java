e6250fa22ab59c14df3568fa955d126e7081f132
SoroushAli <84461814+SoroushAli@users.noreply.github.com>
2021-06-06 02:40:26 -0400

    Initial commit

---
+++ src/main/java/Base/App.java
@@ -0,0 +1,33 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Soroush Alinaghian
+ */
+package Base;
+/*
+Example Output
+
+What is the first number? 10
+What is the second number? 5
+10 + 5 = 15
+10 - 5 = 5
+10 * 5 = 50
+10 / 5 = 2
+
+Constraints
+
+Values coming from users will be strings. Ensure that you convert these values to numbers before doing the math.
+Keep the inputs and outputs separate from the numerical conversions and other processing.
+Generate a single output statement with line breaks in the appropriate spots.
+
+Challenges
+
+Revise the program to ensure that inputs are entered as numeric values. Don’t allow the user to proceed if the value entered is not numeric.
+Don’t allow the user to enter a negative number.
+Break the program into functions that do the computations.
+Implement this program as a GUI program that automatically updates the values when any value changes.
+ */
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
